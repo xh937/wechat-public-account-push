9c4283a2dd36d43388c7880248dea9e910bc89f7
xh937 <112926090+xh937@users.noreply.github.com>
2022-10-26 10:56:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '好妹',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oZbw-5noMYEKRKwLxt6Axz65usYA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
@@ -29,11 +29,11 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '2001', date: '10-27',
+          type: '*生日', name: '好妹', year: '2001', date: '9-11',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '2001', date: '10-27',
+          type: '*生日', name: '峰峰', year: '1998', date: '12-04',
         },
         {
           type: '节日', name: '相识纪念日', year: '2020', date: '11-01',
