87855e62b243d5afeb853532b31c4e4bf5e3dad4
xh937 <112926090+xh937@users.noreply.github.com>
2022-10-26 13:21:53 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '好妹',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oZbw-5noMYEKRKwLxt6Axz65usYA',
+      id: 'oZbw-5mFWYT5kfdGDY88PdvluanY','oZbw-5noMYEKRKwLxt6Axz65usYA'，
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'RQEMQApU4ElKqGuP1KrIhwNNzhnv6GnicOERfi7Yiyg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
